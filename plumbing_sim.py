5573a14e52c724eb879bb645285cdc1a3c958261
Logan Graham <LDGBK6@mail.missouri.edu>
2022-12-03 22:31:19 -0600

    Correctly model component pressure drops

--- plumbing_sim.py
+++ plumbing_sim.py
@@ -8,7 +8,8 @@ import scipy.integrate
 import scipy.stats
 import seaborn
 
-STEP_SIZE = 1  # Spacing in seconds of recorded measurements
+STEP_SIZE = 1e-0  # Spacing in seconds of recorded measurements
+SEARCH_RESOLUTION = 1e-2  # Spacing in kg/s of mass flowrate estimation
 
 FLUID = 'NITROUSOXIDE'
 
@@ -302,49 +303,72 @@ def calculate_all_pressure_drops(mass_flowrate, bottle_pressure):
     bottle_pressure -- pressure in the bottle (Pa)
     """
 
+    defined = True
+
     if not mass_flowrate > 0:
         print(
             f"mass_flowrate ERROR IN calculate_all_pressure_drops ({mass_flowrate})")
-        return {'total': 0, 'tank valve': 0, 'tee 1': 0, 'check valve': 0, 'tee 2': 0, 'ball valve': 0, 'tee 3': 0, 'exit': 0}
+        defined = False
+        return {'defined': defined, 'total': 0, 'tank valve': 0, 'tee 1': 0, 'check valve': 0, 'tee 2': 0, 'ball valve': 0, 'tee 3': 0, 'exit': 0}
 
     if not bottle_pressure > 0:
         print(
             f"bottle_pressure ERROR IN calculate_all_pressure_drops ({bottle_pressure})")
-        return {'total': 0, 'tank valve': 0, 'tee 1': 0, 'check valve': 0, 'tee 2': 0, 'ball valve': 0, 'tee 3': 0, 'exit': 0}
+        defined = False
+        return {'defined': defined, 'total': 0, 'tank valve': 0, 'tee 1': 0, 'check valve': 0, 'tee 2': 0, 'ball valve': 0, 'tee 3': 0, 'exit': 0}
 
     tank_valve_pressure_drop = calculate_valve_pressure_drop(
         mass_flowrate, bottle_pressure, TANK_VALVE_FLOW_COEFFICIENT)
     tank_valve_pressure = bottle_pressure - tank_valve_pressure_drop
 
+    if not tank_valve_pressure > 0:
+        defined = False
+        return {'defined': defined, 'total': 0, 'tank valve': tank_valve_pressure, 'tee 1': 0, 'check valve': 0, 'tee 2': 0, 'ball valve': 0, 'tee 3': 0, 'exit': 0}
     tee_1_pressure_drop = calculate_pressure_drop_from_head_loss(
-        mass_flowrate, tank_valve_pressure if tank_valve_pressure > 0 else bottle_pressure, TEE_LOSS_COEFFICIENT)
+        mass_flowrate, tank_valve_pressure, TEE_LOSS_COEFFICIENT)
     tee_1_pressure = tank_valve_pressure - tee_1_pressure_drop
 
+    if not tee_1_pressure > 0:
+        defined = False
+        return {'defined': defined, 'total': 0, 'tank valve': tank_valve_pressure, 'tee 1': tee_1_pressure, 'check valve': 0, 'tee 2': 0, 'ball valve': 0, 'tee 3': 0, 'exit': 0}
     check_valve_pressure_drop = calculate_valve_pressure_drop(
-        mass_flowrate, tee_1_pressure if tee_1_pressure > 0 else bottle_pressure, CHECK_VALVE_FLOW_COEFFICIENT)
-    check_valve_pressure = tee_1_pressure_drop - check_valve_pressure_drop
+        mass_flowrate, tee_1_pressure, CHECK_VALVE_FLOW_COEFFICIENT)
+    check_valve_pressure = tee_1_pressure - check_valve_pressure_drop
 
+    if not check_valve_pressure > 0:
+        defined = False
+        return {'defined': defined, 'total': 0, 'tank valve': tank_valve_pressure, 'tee 1': tee_1_pressure, 'check valve': check_valve_pressure, 'tee 2': 0, 'ball valve': 0, 'tee 3': 0, 'exit': 0}
     tee_2_pressure_drop = calculate_pressure_drop_from_head_loss(
-            mass_flowrate, check_valve_pressure if check_valve_pressure > 0 else bottle_pressure, TEE_LOSS_COEFFICIENT)
+        mass_flowrate, check_valve_pressure, TEE_LOSS_COEFFICIENT)
     tee_2_pressure = check_valve_pressure - tee_2_pressure_drop
 
+    if not tee_2_pressure > 0:
+        defined = False
+        return {'defined': defined, 'total': 0, 'tank valve': tank_valve_pressure, 'tee 1': tee_1_pressure, 'check valve': check_valve_pressure, 'tee 2': tee_2_pressure, 'ball valve': 0, 'tee 3': 0, 'exit': 0}
     ball_valve_pressure_drop = calculate_pressure_drop_from_head_loss(
-            mass_flowrate, tee_2_pressure if tee_2_pressure > 0 else bottle_pressure, BALL_VALVE_LOSS_COEFFICIENT)
+        mass_flowrate, tee_2_pressure, BALL_VALVE_LOSS_COEFFICIENT)
     ball_valve_pressure = tee_2_pressure - ball_valve_pressure_drop
 
+    if not ball_valve_pressure > 0:
+        defined = False
+        return {'defined': defined, 'total': 0, 'tank valve': tank_valve_pressure, 'tee 1': tee_1_pressure, 'check valve': check_valve_pressure, 'tee 2': tee_2_pressure, 'ball valve': ball_valve_pressure, 'tee 3': 0, 'exit': 0}
     tee_3_pressure_drop = calculate_pressure_drop_from_head_loss(
-            mass_flowrate, ball_valve_pressure if ball_valve_pressure > 0 else bottle_pressure, TEE_LOSS_COEFFICIENT)
+        mass_flowrate, ball_valve_pressure, TEE_LOSS_COEFFICIENT)
     tee_3_pressure = ball_valve_pressure - tee_3_pressure_drop
 
+    if not tee_3_pressure > 0:
+        defined = False
+        return {'defined': defined, 'total': 0, 'tank valve': tank_valve_pressure, 'tee 1': tee_1_pressure, 'check valve': check_valve_pressure, 'tee 2': tee_2_pressure, 'ball valve': ball_valve_pressure, 'tee 3': tee_3_pressure, 'exit': 0}
     exit_pressure_drop = calculate_pressure_drop_from_head_loss(
-            mass_flowrate, tee_3_pressure if tee_3_pressure > 0 else bottle_pressure, EXIT_LOSS_COEFFICIENT)
+        mass_flowrate, tee_3_pressure, EXIT_LOSS_COEFFICIENT)
     exit_pressure = tee_3_pressure - exit_pressure_drop
 
     total_pressure_drop = bottle_pressure - exit_pressure
 
-    return {'total': total_pressure_drop, 'tank valve': tank_valve_pressure_drop, 'tee 1': tee_1_pressure_drop, 'check valve': check_valve_pressure_drop, 'tee 2': tee_2_pressure_drop, 'ball valve': ball_valve_pressure_drop, 'tee 3': tee_3_pressure_drop, 'exit': exit_pressure_drop}
+    return {'defined': True, 'total': total_pressure_drop, 'tank valve': tank_valve_pressure_drop, 'tee 1': tee_1_pressure_drop, 'check valve': check_valve_pressure_drop, 'tee 2': tee_2_pressure_drop, 'ball valve': ball_valve_pressure_drop, 'tee 3': tee_3_pressure_drop, 'exit': exit_pressure_drop}
 
 
+'''
 def calculate_total_pressure_drop_error(mass_flowrate, bottle_pressure, real_pressure_drop):
     """Calculates the error between real and guess in pressure drop through the plumbing system.
 
@@ -373,8 +397,9 @@ def calculate_total_pressure_drop_error(mass_flowrate, bottle_pressure, real_pre
         mass_flowrate, bottle_pressure)['total']
 
     return estimated_pressure_drop - real_pressure_drop
-
 '''
+
+
 def calculate_incompressible_mass_flowrate(bottle_pressure):
     """Calculates the incompressible mass flowrate in m^3/s through the plumbing system.
 
@@ -410,8 +435,24 @@ def calculate_incompressible_mass_flowrate(bottle_pressure):
             f"mass_flowrate_upper, mass_flowrate_lower ERROR IN calculate_incompressible_mass_flowrate ({mass_flowrate_upper}, {mass_flowrate_lower})")
         return 0
 
-    return scipy.optimize.bisect(
-        calculate_total_pressure_drop_error, mass_flowrate_lower, mass_flowrate_upper, args=(bottle_pressure, real_pressure_drop))
+    estimate = None
+    estimate_pressure_drop_error = None
+    for i in numpy.arange(mass_flowrate_lower, mass_flowrate_upper + SEARCH_RESOLUTION, SEARCH_RESOLUTION):
+        if i != 0:
+            pressure_drops_i = calculate_all_pressure_drops(i, bottle_pressure)
+            if pressure_drops_i['defined']:
+                total_pressure_drop_error_i = abs(
+                    pressure_drops_i['total'] - real_pressure_drop)
+                if not estimate_pressure_drop_error or total_pressure_drop_error_i < estimate_pressure_drop_error:
+                    estimate = i
+                    estimate_pressure_drop_error = total_pressure_drop_error_i
+
+    if estimate and estimate_pressure_drop_error:
+        return estimate
+
+    print("ERROR IN calculate_incompressible_mass_flowrate NO ESTIMATE FLOWRATE FOUND")
+    return 0
+
 
 def convert_pa_to_psi(pressure):
     """Converts pressure in pascals into pounds per square inch.
@@ -474,7 +515,6 @@ def calculate_incompressible_mass_flowrate_from_mass(bottle_mass):
 
     bottle_absolute_pressure = calculate_bottle_pressure(bottle_mass[0])
     return calculate_incompressible_mass_flowrate(bottle_absolute_pressure)
-'''
 
 
 def main():
@@ -497,24 +537,6 @@ def main():
     print("The isentropic incompressible flowrate is",
           round(convert_kg_per_s_to_lbm_per_s(isentropic_incompressible_mass_flowrate), 3), "lbm/s")
 
-    x=[]
-    y1=[]  
-    y2=[]      
-    for i in numpy.linspace(0, isentropic_incompressible_mass_flowrate, 1000):
-        x.append(i)
-        y1.append(calculate_total_pressure_drop_error(i, INITIAL_BOTTLE_PRESSURE, INITIAL_BOTTLE_PRESSURE - ATMOSPHERIC_PRESSURE))
-        y2.append(calculate_all_pressure_drops(i, INITIAL_BOTTLE_PRESSURE)['total'])
-
-    matplotlib.pyplot.figure()
-    seaborn.lineplot(x=x, y=y1)
-
-    matplotlib.pyplot.figure()
-    seaborn.lineplot(x=x, y=y2)
-
-    matplotlib.pyplot.show()
-
-    return 0
-
     # Incompressible flow assumption
     incompressible_mass_flowrate = calculate_incompressible_mass_flowrate(
         INITIAL_BOTTLE_PRESSURE)
@@ -527,13 +549,13 @@ def main():
     print("Pressure compare", round(incompressible_pressure_drop, 3) / (10 ** 3), "kPa to",
           round(INITIAL_BOTTLE_PRESSURE - ATMOSPHERIC_PRESSURE, 3) / (10 ** 3), "kPa")
 
-    return 0
-
     final_time = INITIAL_BOTTLE_MASS / incompressible_mass_flowrate * 2
     solution = scipy.integrate.solve_ivp(fun=lambda t, y: -calculate_incompressible_mass_flowrate_from_mass(
-        y, BOTTLE_VOLUME, PIPE_AREA), t_span=(0, final_time), y0=[INITIAL_BOTTLE_MASS], max_step=STEP_SIZE)
+        y), t_span=(0, final_time), y0=[INITIAL_BOTTLE_MASS], max_step=STEP_SIZE)
 
     bottle_mass_list = solution.y[0]
+
+    print("IVP solved.")
 
     y_bottle_mass = []
     y_bottle_pressure = []
@@ -541,24 +563,13 @@ def main():
     y_ball_valve_pressure_drop = []
     i = 0
     while i < len(solution.y[0]):
-        if i == 0:
-            i_mass_flowrate = - \
-                scipy.stats.linregress(
-                    x=solution.t[i:i+2], y=bottle_mass_list[i:i+2]).slope
-        elif i == (len(solution.y[0]) - 1):
-            i_mass_flowrate = - \
-                scipy.stats.linregress(
-                    x=solution.t[i-2:i], y=bottle_mass_list[i-2:i]).slope
-        else:
-            i_mass_flowrate = - \
-                scipy.stats.linregress(
-                    x=solution.t[i-1:i+1], y=bottle_mass_list[i-1:i+1]).slope
+        i_mass_flowrate = calculate_incompressible_mass_flowrate_from_mass([
+                                                                           bottle_mass_list[i]])
 
         y_mass_flowrate.append(convert_kg_to_lbm(i_mass_flowrate))
 
         y_bottle_mass.append(convert_kg_to_lbm(bottle_mass_list[i]))
-        i_bottle_pressure = calculate_bottle_pressure(
-            BOTTLE_VOLUME, bottle_mass_list[i])
+        i_bottle_pressure = calculate_bottle_pressure(bottle_mass_list[i])
         y_bottle_pressure.append(convert_pa_to_psi(
             convert_absolute_pressure_to_gauge(i_bottle_pressure)))
 
@@ -572,6 +583,7 @@ def main():
     df = pandas.DataFrame(data={'Time (s)': solution.t, 'Bottle Fluid Mass (lbm)': y_bottle_mass, 'Mass Flowrate (lbm/s)': y_mass_flowrate,
                                 'Bottle Gauge Pressure (psi)': y_bottle_pressure, 'Ball Valve Pressure Drop (psi)': y_ball_valve_pressure_drop})
     print(df)
+    df.to_excel("output.xlsx")
 
     matplotlib.pyplot.figure()
     seaborn.lineplot(x='Time (s)', y='Bottle Fluid Mass (lbm)', data=df)
